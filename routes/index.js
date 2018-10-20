ad864f081269d695c40e42d8d272492f8518d899
Dennis <tr.ainer036@googlemail.com>
2018-10-20 17:48:05 +0200

    Add register and login pages with register-/login-forms.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET home page
 router.get('/', function(req, res) {
   res.render('home', { title: 'Home' });
+});
+
+// GET profile page
+router.get('/profile', function(req, res, next) {
+  res.render('profile', { title: 'Profile' });
+});
+
+// GET login page
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+// GET register page
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Registration' });
 });
 
 module.exports = router;
