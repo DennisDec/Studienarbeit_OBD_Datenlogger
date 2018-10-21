07e6a27d518123921410e374119b93a4b9ead9b4
Dennis <tr.ainer036@googlemail.com>
2018-10-21 12:46:41 +0200

    Import markers from json-file and print them on map.

--- src/maps/leaf.js
+++ src/maps/leaf.js
@@ -7,3 +7,15 @@ var map = L.map( 'map', {
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+var printMarkers = async function() {
+    let response = await fetch("maps/markers.json");
+    let markers = await response.json();
+
+    for ( var i=0; i < markers.length; ++i ) 
+    {
+    L.marker( [markers[i].lat, markers[i].lng] )
+        .bindPopup( '<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>' )
+        .addTo( map );
+    }
+}
+printMarkers();
