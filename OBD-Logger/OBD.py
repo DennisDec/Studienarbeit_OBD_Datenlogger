37b51021980efa359f6792d598bc696f52d22bb5
Pascal <Pascal@PASCAL-PC>
2018-10-25 17:07:51 +0200

    Bug Fixes

--- OBD-Logger/OBD.py
+++ OBD-Logger/OBD.py
@@ -17,53 +17,63 @@ while NotConnected:
     connection = obd.OBD()
 
     print(connection.status())
-    if(connection.status() == obd.utils.OBDStatus.CAR_CONNECTED):
+    if(connection.status() == obd.utils.OBDStatus.CAR_CONNECTED and (connection.query(obd.commands.RPM).is_null() == False)):
         NotConnected = False
     else:
-        time.sleep(3)
+        time.sleep(2)
 
+
+print("Erfolg")
 
 PidsMode1 = [i for i in PidsMode1 if obd.commands.has_pid(1, i)]
 
 
-HeaderCSV = ["Time", "Speed", "RPM"]
+HeaderCSV = ["Time", "Speed", "RPM", "Engine_Load", "MAF", "Temperature", "Pedal", "AFR", "Fuel Level"]
 log = LogFile()
 log.createLogfile(filename, HeaderCSV)
 
-IgnitionStep2 = False
+IgnitionStep2 = True
 while (IgnitionStep2 == False):
     
+    #connection = obd.OBD()
     if(connection.query(obd.commands.RPM).is_null() == False):
         IgnitionStep2 = True
-    time.sleep(3)
+    print("Ignition not on Step 2")
+    time.sleep(2)
     
 while (connection.status() == obd.utils.OBDStatus.CAR_CONNECTED and (connection.query(obd.commands.RPM).is_null() == False)):
     
     i = i+1
     timestr = str(datetime.datetime.now()) #Parse String to datetime :  t = datetime.datetime.strptime(t,"%Y-%m-%d %H:%M:%S.%f") --> Calculate Difference: t = t2-t1 --> t = round(t.total_seconds()/60, 2) [Time in Minute]
-    speed = connection.query(obd.commands.SPEED)
-    rpm = connection.query(obd.commands.RPM)
-
-    if speed.is_null():
-        speed = 0
-    else:
-        speed = speed.value.magnitude
     
-    if rpm.is_null():
-        rpm = 0
-    else:
-        rpm = rpm.value.magnitude
+    result = []
+    result.append(connection.query(obd.commands.SPEED))
+    result.append(connection.query(obd.commands.RPM))
+    result.append(connection.query(obd.commands.ENGINE_LOAD))
+    result.append(connection.query(obd.commands.MAF))
+    result.append(connection.query(obd.commands.AMBIANT_AIR_TEMP))
+    result.append(connection.query(obd.commands.RELATIVE_ACCEL_POS))
+    result.append(connection.query(obd.commands.COMMANDED_EQUIV_RATIO))
+    result.append(connection.query(obd.commands.FUEL_LEVEL))
+    
+    res = []
+    for r in result:
+        if r.is_null():
+            res.append(0)
+        else:
+            res.append(r.value.magnitude)
 
-    log.addData(timestr, speed, rpm)
+    
+    log.addData(timestr, res[0], res[1], res[2], res[3], res[4], res[5], res[6], res[7])
     time.sleep(1)
 
     if(i%10 == 0):
         log.appendFile()
+        print("Append File")
 
 print("Zündung wurde ausgeschaltet!\n")
    
-
-
+    
 
     
 
