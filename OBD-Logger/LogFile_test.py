c6af96abb72b99edb1d8e2cdf80416dda734a9b1
Pascal <Pascal@PASCAL-PC>
2018-10-26 15:38:42 +0200

    New Testcases in LogFile_test.py

--- OBD-Logger/LogFile_test.py
+++ OBD-Logger/LogFile_test.py
@@ -7,7 +7,6 @@ import datetime
 filename = 'Unittest.csv'
 
 
-
 class TestLogFile(unittest.TestCase):
 
     def test_StatusNoLogFile(self):
@@ -30,21 +29,45 @@ class TestLogFile(unittest.TestCase):
 
     def test_IsCorrectSpeedList(self):
         log = LogFile()
-        log.createLogfile(filename, ["Time", "Speed", "RPM"])
+        log.createLogfile(filename, ["Time", "Speed", "RPM", "Engine_Load",
+             "MAF", "Temperature", "Pedal", "AFR", "Fuel Level"])
 
         t1 = str(datetime.datetime.now())
-        t2 = str(datetime.datetime.now())
 
-        log.addData(t1, 30, 2400, 12, 2, 4, 5, 6, 7)
-        log.addData(t2, 40, 2600, 23 ,24, 34 , 3, 6, 67)
+        speed = [34, 35, 36, 24, 23, 56]
+        rpm = [2000, 2001, 2100, 3100, 50000, 60000]
+        load = [60, 40, 50, 30, 70, 80]
+        maf = [3, 4, 3, 4, 3, 2]
+        temp = [13,13,13,14,14,14]
+        pedal = [30,33,24,36,21, 18]
+        afr =[1, 1.2, 1.1, 1, 1, 1]
+        fuel_lvl = [80, 80, 80, 79, 79, 79]
+
+        for i in range(0, len(speed)):
+            log.addData(t1, speed[i], rpm[i], load[i], maf[i], temp[i], pedal[i], afr[i], fuel_lvl[i])
 
         log.appendFile()
 
         log2 = LogFile()
         log2.loadFromFile(filename)
 
-        speed = log2.getLabelData(SupportedLabels.SPEED)
-        self.assertEqual(speed, [30, 40])
+        speed_csv = log2.getLabelData(SupportedLabels.SPEED)
+        afr_csv = log2.getLabelData(SupportedLabels.COMMANDED_EQUIV_RATIO)
+        temp_csv = log2.getLabelData(SupportedLabels.AMBIANT_AIR_TEMP)
+        rpm_csv = log2.getLabelData(SupportedLabels.RPM)
+        load_csv = log2.getLabelData(SupportedLabels.ENGINE_LOAD)
+        maf_csv = log2.getLabelData(SupportedLabels.MAF)
+        pedal_csv = log2.getLabelData(SupportedLabels.RELATIVE_ACCEL_POS)
+        fuel_lvl_csv = log2.getLabelData(SupportedLabels.FUEL_LEVEL)
+
+        self.assertEqual(speed_csv, speed)
+        self.assertEqual(afr_csv, afr)
+        self.assertEqual(temp_csv, temp)
+        self.assertEqual(rpm_csv, rpm)
+        self.assertEqual(load_csv, load)
+        self.assertEqual(maf_csv, maf)
+        self.assertEqual(pedal_csv, pedal)
+        self.assertEqual(fuel_lvl_csv, fuel_lvl)
 
 
 if __name__ == '__main__':
