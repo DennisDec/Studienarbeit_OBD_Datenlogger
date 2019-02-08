967fe4e0087ebb0f3550c01d4c085b91d13d72ac
Dennis <tr.ainer036@googlemail.com>
2019-02-08 19:36:12 +0100

    Let transferToJson function return path of file and fix bugs.

--- OBD-Logger/LogFile.py
+++ OBD-Logger/LogFile.py
@@ -68,13 +68,14 @@ class LogFile:
 
     @staticmethod
     def transferToJson(filename):
-        jsonPath = "OBD-Logger\\Files\\JSON\\"
+        jsonPath = path + "\\JSON\\"
         with open(path +filename, 'r') as csvfile:
             next(csvfile)
             fileReader = csv.DictReader(csvfile, fieldnames=("TIME", "SPEED", "RPM", "ENGINE_LOAD", "MAF" ,"AMBIANT_AIR_TEMP", "RELATIVE_ACCEL_POS", "COMMANDED_EQUIV_RATIO","FUEL_LEVEL" ,"GPS_Long", "GPS_Lat"))
             out = json.dumps( [ row for row in fileReader ] ) 
-            f = open( jsonPath + filename + ".json", 'w')  
-            f.write(out)    
+            f = open( jsonPath + filename.split(".csv")[0] + ".json", 'w')  
+            f.write(out)
+            return filename.split(".csv")[0] + ".json"    
 
     @staticmethod
     def copyFileToServer(filename):
@@ -93,7 +94,7 @@ class LogFile:
                 ip.append(output[i-3].split(' ')[-1])
                 print(ip)
         for i, tmp in enumerate(ip):
-            os.system("sshpass -p '" + str(env.DB_PASSWORD) + "' scp " + str(path) + str(filename) + " pi@" + str(ip[i]) + ":datafiles/")
+            os.system("sshpass -p '" + str(env.DB_PASSWORD) + "' scp " + str(path) + "JSON\\" + str(filename) + " pi@" + str(ip[i]) + ":datafiles/")
 
 
 
