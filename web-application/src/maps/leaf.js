e9edc53e60e050d05fb2edf5f63e8a4547adb601
Dennis <tr.ainer036@googlemail.com>
2019-02-09 17:57:14 +0100

    Remove old map markers.

--- web-application/src/maps/leaf.js
+++ web-application/src/maps/leaf.js
@@ -1,13 +1,19 @@
-
 var map = L.map( 'map', {
     center: [20.0, 5.0],
     minZoom: 2,
     zoom: 2
 });
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+
 var printMarkers = async function(filename) {
+    map.remove();
+    map = L.map( 'map', {
+        center: [20.0, 5.0],
+        minZoom: 2,
+        zoom: 2
+    });
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
     /*let response = await fetch("maps/markers.json");
     let markers = await response.json();*/
     let response = await fetch("/getGPS/" + filename, {
