7b9a83529ea595b45eef9ba7e57261526c4d9e54
Pascal <Pascal@PASCAL-PC>
2019-02-28 00:45:26 +0100

    added new class for utility methods

---
+++ OBD-Logger/Util.py
@@ -0,0 +1,23 @@
+
+from datetime import datetime, timedelta
+
+class Util:
+    """ This class contains different util methods """
+
+    @staticmethod
+    def isfloat(value):
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
+
+    @staticmethod
+    def mean(value):
+        """ calculate mean value of an array"""
+        return (sum(value) / float(len(value)))
+
+    @staticmethod
+    def test(value):
+       str = "123.432"
+       int(float(str))
